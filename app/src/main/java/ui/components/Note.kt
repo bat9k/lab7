674908cfdfe72bcb58f6f08ab33a327d4bef3141
bat9k <ilia34775@gmail.com>
2024-04-12 15:11:23 +0300

    create note

---
+++ app/src/main/java/ui/components/Note.kt
@@ -0,0 +1,13 @@
+package ui.components
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable fun Note(){
+
+}
+@Preview
+@Composable
+private fun NotePreview(){ Note()
+}
