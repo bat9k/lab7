e129aef7696e649ef3de3f2b23f70b8d601e1e69
bat9k <ilia34775@gmail.com>
2024-04-16 00:57:28 +0300

    Add Background to Note

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -9,28 +9,42 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.topic2.android.notes.theme.rwGreen
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Shape
 
 @Composable
-fun Note(){
-    Row(modifier = Modifier.fillMaxWidth()) {
-        NoteColor(
-            color = rwGreen,
-            size =40.dp,
-            padding =4.dp,
-            border=1.dp
-        )
-        Column(modifier = Modifier.weight(1f)) {
-            Text(text = "Заголовок", maxLines = 1)
-            Text(text = "Содержимое", maxLines = 1)
+fun Note() {
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .shadow(1.dp, backgroundShape)
+            .fillMaxWidth()
+            .heightIn(min = 64.dp)
+            .background(Color.White, backgroundShape)
+    )
+    {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            NoteColor(
+                color = rwGreen,
+                size = 40.dp,
+                padding = 4.dp,
+                border = 1.dp
+            )
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Заголовок", maxLines = 1)
+                Text(text = "Содержимое", maxLines = 1)
+            }
+            Checkbox(
+                checked = false,
+                onCheckedChange = { },
+                modifier = Modifier.padding(start = 8.dp)
+            )
         }
-        Checkbox(
-            checked = false,
-            onCheckedChange = { },
-            modifier = Modifier.padding(start = 8.dp)
-        )
     }
 }
 @Preview
 @Composable
-private fun NotePreview(){ Note()
-}
+private fun NotePreview(){ Note()}
