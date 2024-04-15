5ec8fabc548c26c854f5461b319940677b004bb5
bat9k <ilia34775@gmail.com>
2024-04-16 00:47:48 +0300

    Create modifier in NoteColor

--- app/src/main/java/ui/components/NoteColor.kt
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -4,15 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun NoteColor(){
     Box(
-        modifier = Modifier.size(40.dp).background(Color.Red)
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(Color.Red)
     )
 }
 
