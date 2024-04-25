e457827ead34ebe0b8e8ba221fcc2785b19481a3
bat9k <ilia34775@gmail.com>
2024-04-25 15:33:31 +0300

    Stateless Composite Objects

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -17,9 +17,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.util.fromHex
 
 @Composable
-fun Note(){
+fun Note(
+    note: NoteModel,
+    onNoteClick: (NoteModel) -> Unit = {},
+    onNoteCheckedChange: (NoteModel) -> Unit = {}
+){
     val backgroundShape: Shape = RoundedCornerShape(4.dp)
     Row(modifier = Modifier
         .padding(8.dp)
@@ -32,42 +38,45 @@ fun Note(){
             modifier = Modifier
                 .align(Alignment.CenterVertically)
                 .padding(start = 16.dp, end = 16.dp),
-            color = rwGreen,
+            color = Color.fromHex(note.color.hex),
             size =40.dp,
-            padding =4.dp,
             border=1.dp
         )
+
         Column(modifier = Modifier.weight(1f).align(Alignment.CenterVertically))
-            { Text(text = "Заголовок",
-                color = Color.Black,
+        { Text(text = note.title,
+            color = Color.Black,
+            maxLines = 1,
+            style = TextStyle(
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                letterSpacing = 0.15.sp
+            )
+        )
+            Text(
+                text = note.content,
+                color = Color.Black.copy(alpha = 0.75f),
                 maxLines = 1,
                 style = TextStyle(
                     fontWeight = FontWeight.Normal,
-                    fontSize = 16.sp,
-                    letterSpacing = 0.15.sp
+                    fontSize = 14.sp,
+                    letterSpacing = 0.25.sp
                 )
             )
-                Text(
-                    text = "Содержание",
-                    color = Color.Black.copy(alpha = 0.75f),
-                    maxLines = 1,
-                    style = TextStyle(
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 14.sp,
-                        letterSpacing = 0.25.sp
-                    )
-                )
         }
-        Checkbox(
-            checked = false,
-            onCheckedChange = { },
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        )
+
+        if(note.isCheckedOff !=null)
+
+            Checkbox(
+                checked = note.isCheckedOff,
+                onCheckedChange = { },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+            )
     }
 }
-
 @Preview
 @Composable
-private fun NotePreview(){ Note()}
+private fun NotePreview(){ Note(note = NoteModel(1, "Заметка 228", "Содержание", null))
+}
